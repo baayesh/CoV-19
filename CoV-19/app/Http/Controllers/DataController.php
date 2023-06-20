9e239372c86ce19272ee3e46e456bf23ebf18b36
baayesh <ayeshisurukavinda.1998@gmail.com>
2023-06-21 00:08:45 +0530

    add new id row to table

--- CoV-19/app/Http/Controllers/DataController.php
+++ CoV-19/app/Http/Controllers/DataController.php
@@ -5,50 +5,72 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\covid_19_detail;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 
 
 
 class DataController extends Controller
 {
-   
+    //get values from database 
     public function read()
     {
-        $latestValue = covid_19_detail::max('created_at');
-        $data = covid_19_detail::where('created_at', $latestValue) -> first();
 
-        return view ('home', ['detail' => $data]);
+
+           DB::connection()->getPdo();
+           if(DB::connection()->getDatabaseName()){
+                
+                $latestValue = covid_19_detail::max('id');
+                $data = covid_19_detail::where('id', $latestValue) -> first();
+        
+               if(!$data){
+                    return redirect() ->route('UpdateData');
+               }
+                
+                return view ('home', ['detail' => $data]);
+           }
+
+
     }
    
     public function store()
    {
- 
-    $api_url = 'https://hpb.health.gov.lk/api/get-current-statistical';
-    $res = Http::timeout(200000)->get($api_url);
-    $data = json_decode($res -> body());
-    $dataArray = (array)$data->data;
+    //Fetch data from api
+    try{
+        $api_url = 'https://hpb.health.gov.lk/api/get-current-statistical';
+        $res = Http::timeout(200000)->get($api_url);
+        $data = json_decode($res -> body());
+        $dataArray = (array)$data->data;
+    }
+    catch(\Exception){
+        return redirect() ->route('home');
+    }
 
-    $covid_19_detail = new covid_19_detail;
+  
+    //Insert data to the database comes form api
+    $covid19Data = [
+        'local_new_cases' => $dataArray['local_new_cases'],
+        'local_total_cases' => $dataArray['local_total_cases'],
+        'number_of_individuals_in_hospitals' => $dataArray['local_total_number_of_individuals_in_hospitals'],
+        'local_deaths' => $dataArray['local_deaths'],
+        'local_new_deaths' => $dataArray['local_new_deaths'],
+        'local_recovered' => $dataArray['local_recovered'],
+        'local_active_cases'=> $dataArray['local_active_cases'],
+        'global_new_cases' => $dataArray['global_new_cases'],
+        'global_total_cases' => $dataArray['global_total_cases'],
+        'global_deaths' => $dataArray['global_deaths'],
+        'global_new_deaths' => $dataArray['global_new_deaths'],
+        'global_recovered' => $dataArray['global_recovered'],
+        'total_pcr_testing_count' => $dataArray['total_pcr_testing_count']
 
-    $covid_19_detail -> local_new_cases = $dataArray['local_new_cases'];
-    $covid_19_detail -> local_total_cases = $dataArray['local_total_cases'];
-    $covid_19_detail -> number_of_individuals_in_hospitals = $dataArray['local_total_number_of_individuals_in_hospitals'];
-    $covid_19_detail -> local_deaths = $dataArray['local_deaths'];
-    $covid_19_detail -> local_new_deaths = $dataArray['local_new_deaths'];
-    $covid_19_detail -> local_recovered = $dataArray['local_recovered'];
-    $covid_19_detail -> local_active_cases= $dataArray['local_active_cases'];
-    $covid_19_detail -> global_new_cases = $dataArray['global_new_cases'];
-    $covid_19_detail -> global_total_cases = $dataArray['global_total_cases'];
-    $covid_19_detail -> global_deaths = $dataArray['global_deaths'];
-    $covid_19_detail -> global_new_deaths = $dataArray['global_new_deaths'];
-    $covid_19_detail -> global_recovered = $dataArray['global_recovered'];
-    $covid_19_detail -> total_pcr_testing_count = $dataArray['total_pcr_testing_count'];
+    ];
 
-    $covid_19_detail->save();
+    covid_19_detail::insert($covid19Data);
 
     return redirect() ->route('delete');
 
    }
 
+   //Delete Old Data
    public function destroy()
    {
     $oldestValue = covid_19_detail::min('created_at');
